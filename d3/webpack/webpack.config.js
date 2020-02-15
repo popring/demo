6f318614fe1b66b3cdf7961f20425381b1809486
HarryHao <koler778@gmail.com>
2020-02-15 09:46:34 +0800

    webpack init

---
+++ d3/webpack/webpack.config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
